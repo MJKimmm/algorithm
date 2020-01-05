f468e46a43b6a6a09d7158f5c3d9b52148da30ef
MJKimmm <mijin1940@sookmyung.ac.kr>
2020-01-06 01:49:49 +0900

    greedy algorithm_seminar

--- src/main/java/seminar_1931.java
+++ src/main/java/seminar_1931.java
@@ -18,18 +18,26 @@ public class seminar_1931 {
             arr[i][1] = b;
         }
 
+
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                for(int i=0; i<N; i++) {
-                    if(arr[i][1] > arr[i+1][1]) {
-                        return Integer.compare(o1[0], o2[0]); //////////////
-                    }
-                }
-                return Integer.compare(o1[0], o2[0]);
-
+                if (o1[1] == o2[1]) return o1[0] - o2[0];
+                return o1[1] - o2[1];
             }
         });
+
+        int cnt = 1;
+        int end = arr[0][1];
+        for(int i=0; i<N-1; i++) {
+
+            if (end < arr[i+1][0] || end == arr[i+1][0]) {
+                cnt++;
+                end = arr[i+1][1];
+            }
+        }
+
+        System.out.println("cnt : " + cnt);
 
     }
 }
