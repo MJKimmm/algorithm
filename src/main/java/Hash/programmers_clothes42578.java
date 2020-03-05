6b55afe5c36a15e64ed8cafeee19390e0de8be0c
MJKimmm <mijin1940@sookmyung.ac.kr>
2020-03-06 01:17:14 +0900

    programmers_hash_clothes

---
+++ src/main/java/Hash/programmers_clothes42578.java
@@ -0,0 +1,47 @@
+package Hash;
+
+import java.util.HashMap;
+
+public class programmers_clothes42578 {
+}
+
+class Solution2 {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+
+        HashMap<String, String> hm = new HashMap<String, String>();
+
+        int n = clothes.length;
+/*
+        for(int i=0; i<n; i++) {
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) +1);
+        }
+
+        answer += n;
+
+        for(String a: hm.keySet()) {
+            System.out.println(hm.get(a));
+            System.out.println(hm.get(a));
+        } */
+
+        for(int i=0; i<n; i++) {
+            hm.put(clothes[i][0], clothes[i][1]); // key : 옷이름 value : 옷종류
+            answer++;
+        }
+
+        System.out.println(answer);
+
+        int m=0;
+        for(String a:hm.keySet()) {
+            for(String b:hm.keySet()) {
+                if(hm.get(a) != hm.get(b) && a!=b) {
+                    System.out.println(a + b);
+                    m++;
+                }
+            }
+        }
+        answer += m/2;
+
+        return answer;
+    }
+}
