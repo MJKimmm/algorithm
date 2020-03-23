456ebdd4ddcfc8a8b931539244fbf09db3191d6a
MJKimmm <mijin1940@sookmyung.ac.kr>
2020-03-23 20:53:01 +0900

    programmers_Queue_bridgeTruck

--- src/main/java/Programmers/StackQueue/bridgeTruck42583.java
+++ src/main/java/Programmers/StackQueue/bridgeTruck42583.java
@@ -6,9 +6,9 @@ import java.util.Queue;
 public class bridgeTruck42583 {
 
     public static void main(String[] args) {
-        int bridge_length = 100;
-        int weight = 100;
-        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
 
         System.out.println(Solution6.solution(bridge_length, weight, truck_weights));
     }
@@ -16,43 +16,47 @@ public class bridgeTruck42583 {
 
 class Solution6 {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 0;
 
-        Queue<Integer> ingTruck = new LinkedList<>();
-        Queue<Integer> waitTruck = new LinkedList<>();
+        Queue<Truck> waitTruck = new LinkedList<>();
+        Queue<Truck> ingTruck = new LinkedList<>();
 
-        for(int i=0; i<truck_weights.length; i++) {
-            waitTruck.add(truck_weights[i]);
+        for(int w : truck_weights) {
+            waitTruck.add(new Truck(w, 0));
         }
 
         int total = 0;
-        int time = 0;
-        int length = 0;
-
-        total += waitTruck.peek(); /////////////
+        total += waitTruck.peek().weight;
         ingTruck.add(waitTruck.poll());
 
         while(!ingTruck.isEmpty()) {
             time++;
-            for(int i=0; i<ingTruck.size(); i++) {
-                length++;
-            }
-            //System.out.println("time" + time + "len" + length);
 
-            if(length>bridge_length) {
-                total -= ingTruck.poll();
-                if(ingTruck.isEmpty()) {
-                    length=0;
-                }
+            for(Truck t : ingTruck) {
+                t.distance++;
             }
 
-            if(!waitTruck.isEmpty() && waitTruck.peek()+total <= weight) {
-                total += waitTruck.peek();
-                length++;
+            if(ingTruck.peek().distance > bridge_length) {
+                total -= ingTruck.poll().weight;
+            }
+
+            if(!waitTruck.isEmpty() && total+waitTruck.peek().weight <= weight) {
+                total += waitTruck.peek().weight;
+                waitTruck.peek().distance++;
                 ingTruck.add(waitTruck.poll());
             }
         }
 
-
         return time;
+    }
+}
+
+class Truck {
+    int weight;
+    int distance;
+
+    public Truck(int weight, int distance) {
+        this.weight=weight;
+        this.distance=distance;
     }
 }
